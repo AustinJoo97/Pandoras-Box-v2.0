3f880b716cfc8cabf7c08a6a1bb2e7490fcdc978
AustinJoo97 <austinjoo97@gmail.com>
2021-08-19 18:08:10 -0400

    migrated comment model from v1.0

---
+++ server/models/Comment.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema = new Schema({
+    commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 520
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp)
+    },
+    postedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    albumCommented: {
+        type: Schema.Types.ObjectId,
+        ref: 'Album'
+    }
+})
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
