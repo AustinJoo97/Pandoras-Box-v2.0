ec20539a1f50bf2135225d2ca51f93ee2b70827d
AustinJoo97 <austinjoo97@gmail.com>
2021-09-15 13:33:04 -0400

    implemented initial code for establishing GQL-Apollo server at route '/api/graphql'; will need to test to determine if this successfully connects to mongoDB; afterwards, figure out how to apply authentication middleware

--- pandoras-box/pages/api/graphql.js
+++ pandoras-box/pages/api/graphql.js
@@ -9,3 +9,17 @@ export const server = makeExecutableSchema({
     typeDefs,
     resolvers
 });
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+export default new ApolloServer({ 
+    server,
+    // This line of code may not be functional at the moment; need to find how to apply authMiddleware context to the entire server
+    // context: authMiddleware
+}).createHandler({
+    path: '/api/graphql'
+})
