39c3f00180ab89f3eb5956aa9b8f7f69027695f9
AustinJoo97 <austinjoo97@gmail.com>
2021-09-15 13:29:31 -0400

    called imported connection function to open connecton to db; added server variable to run imported makeExecutableSchema function

--- pandoras-box/pages/api/graphql.js
+++ pandoras-box/pages/api/graphql.js
@@ -2,3 +2,10 @@ import { ApolloServer, makeExecutableSchema } from 'apollo-server-micro';
 import { typeDefs, resolvers } from '../../db/schemas';
 import connection from '../../db/config';
 import { authMiddleware } from '../../db/utils/auth';
+
+connection();
+
+export const server = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
